33f10f838a0340d882cd3e0bff2dd1fd8e80f1a5
Aashiq <aashiq2691@outlook.com>
2021-07-29 01:14:35 +0530

    Basic unit testing

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed } from '@angular/core/testing';
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+
+describe('CalculatorService', () => {
+
+    let calculator: CalculatorService,
+        loggerSpy: any;
+
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+        calculator = TestBed.inject(CalculatorService);
+    });
+
+    it('should add two numbers', () => {
+        // act
+        const result = calculator.add(2, 2);
+
+        // assert
+        expect(result).toBe(4);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should substract two numbers', () => {
+        // act
+        const result = calculator.subtract(2, 2);
+
+        // assert
+        expect(result).toBe(0, "unexpected subraction result");
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+});
